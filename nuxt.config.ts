6610849b3cb492c69a2331d7d9bbf11245ca55b0
hakolr <m.gayvoronskiy@flang.io>
2026-04-09 23:39:12 +0300

    feat: added i18next

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: [
+    "@nuxt/test-utils/module",
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+  ],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
-  routeRules: {
-    '/': { prerender: true }
-  },
-
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+
+  i18n: {
+    defaultLocale: "ru",
+    strategy: "no_prefix",
+    lazy: true,
+    langDir: "locales",
+    locales: [
+      { code: "ru", language: "ru-RU", name: "Русский", file: "ru.json" },
+      { code: "en", language: "en-US", name: "English", file: "en.json" },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_locale",
+      redirectOn: "all",
+    },
+  },
+});
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineVitestProject } from "@nuxt/test-utils/config";
+
+export default defineVitestProject({
+  test: {
+    environment: "nuxt",
+    environmentOptions: {
+      nuxt: {
+        overrides: {
+          i18n: {
+            lazy: false,
+          },
+        },
+      },
+    },
+  },
+});
